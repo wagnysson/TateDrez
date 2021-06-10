f55495a9a3541a4bebbfbb4d855006cdfd8e0e3b
wagnysson <wagnyssonmoura@gmail.com>
2021-06-10 11:18:23 -0300

    nomes dos players, pecas em numeros

---
+++ tic-tac-chec.c
@@ -0,0 +1,36 @@
+// Wagnysson Moura Luz - 12558471
+// Bruno Dias -
+
+#include <stdio.h>
+
+int main(){
+    //declarando as varaveis
+    char j1[20], j2[20]; // recebe o nome dos jogadores
+    int pecasj1[3], pecasj2[3]; //recebe as pecas de cada jogador 
+    int tabuleiro[3][3];
+    int i; //contador
+
+    //informacoes sobre o jogo
+    printf("Bem vindos ao Tic Tac Chec! ");
+
+    //jogadores dão entrada em seus nomes
+    printf("Jogador 1, digite como deseja ser chamado: ");
+    scanf("%s", j1);
+    printf("Jogador 2, digite como deseja ser chamado: ");
+    scanf("%s", j2);
+
+    //ver quais os nomes dos jogadores
+    printf("Jogador 1: %s\n", j1);
+    printf("Jogador 2: %s\n", j2);
+
+    //pecas, 
+    for (i=0; i<3; i++){
+        pecasj1[i] = i;
+    }
+    for (i=3; i<6; i++){
+        pecasj2[i] = i;
+    }
+
+    
+    return(0);
+}
